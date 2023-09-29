67621d0423ecc98d717cec3d283d4d276e8f4173
Giovanny Cordeiro <Giovannycordeiropb@gmail.com>
2023-09-29 12:03:47 -0300

    setup: start project

---
+++ src/test/kotlin/com/giovanny/APIRest/ApiRestApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.giovanny.APIRest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiRestApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
